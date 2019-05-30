bd2e9d825f66a506d1fbe141c45e5c32e52a802b
mvcodeclub <sarah@mvcodeclub.com>
2019-05-29 19:32:27 -0700

    asdf

--- Pendulum.js
+++ Pendulum.js
@@ -1,18 +1,38 @@
 class Pendulum {
-  constructor (startX, startY) {
-    this.mass = 5
-    this.startX = startX;
-    this.startY = startY;
-    this.v = mass * grav;
-    this.angularAcceleration = 0;
+  constructor () {
+    this.mass = 5000;
+    this.velocity = 0;
+    this.acceleration = 0;
+    this.force = 0;
+    this.friction = 0.999;
+    this.angle = PI/3;
+    this.x;
+    this.y;
   }
 
-  draw (x, y, w, h) {
+  move () {
+    this.force = -1 * sin(this.angle) * grav * this.mass;
+    this.acceleration = this.force / this.mass;
+    this.velocity += this.acceleration;
+    this.velocity *= this.friction;
+    this.angle+=this.velocity;
+    this.x = 250 + 200 * sin(this.angle);
+    this.y =  50 + 200 * cos(this.angle);
+  }
+
+  render () {
     fill(30,155,255);
-    ellipse(x, y, w, h);
+    ellipse(this.x, this.y, this.mass/100, this.mass/100);
   }
 
+  drawLine () {
+    line(250,50,this.x, this.y)
+  }
   update () {
-    this.draw();
+    this.move();
+    this.render();
+    this.drawLine();
+    //console.log(this.x)
+    //console.log(this.y)
   }
 }
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+console.clear();
+var p;
+
 var originX = 250;
 var originY = 50;
 
-var xPos = 250;
-var yPos = 250;
 
-var grav = 9.8;
+var grav = 0.001;
 
 function setup () {
   createCanvas(500,500);
+  p = new Pendulum();
 }
 
 function draw () {
   background(200);
-  line(originX, originY, xPos, yPos);
-  ellipse(xPos, yPos, 25, 25);
+  fill(100);
+  rect(0,40,500,10);
+  p.update();
 }
